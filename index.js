87579cd3f16e448987467c4c5a4c99bd9812afcb
Jordan <sommygames@gmail.com>
2025-05-02 13:04:39 +0100

    Hints 1 to 3 done.

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+const API_URL = "https://secrets-api.appbrewery.com";
+
+
+
 // HINTS:
 // 1. Import express and axios
 
