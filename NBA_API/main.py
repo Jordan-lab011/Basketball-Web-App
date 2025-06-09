51b4653c1bb01da14873148267c46f92d23f1e40
Jordan <sommygames@gmail.com>
2025-06-09 17:24:04 +0100

    NBA API integrated into setup and tested, but nothing has been done.

--- NBA_API/main.py
+++ NBA_API/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from nba_api.stats.static import players
 
 app = FastAPI()
@@ -6,3 +6,11 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "NBA Stats API is running."}
+
+@app.get("/searchPlayer/{name}")
+def search_player(name: str):
+    matches = players.find_players_by_full_name(name)
+    if not matches:
+        raise HTTPException(status_code=404, detail="Player not found")
+    player = matches[0]
+    return {"playerId": player["id"], "fullName": player["full_name"]}
--- index.js
+++ index.js
@@ -4,13 +4,30 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+const API_URL = "http://127.0.0.1:8000"
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"))
 
-app.get("/", (req, res) => { // : Route path (e.g., 'users')
-  res.render("index.ejs")
+app.get("/", async(req, res) => { 
+  try {
+      const response = await axios.get(API_URL); 
+      const data = (response.data); 
+      console.log(data);
+      res.render("index.ejs", {
+        user: data.username,
+        secret: data.secret
+    });
+  
+    } catch (error) {
+      console.error("Request failed:", error.message);
+      res.render("index.ejs", {
+        error: error.message,
+      });
+    }
 });
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
